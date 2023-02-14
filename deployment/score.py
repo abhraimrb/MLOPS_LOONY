4c187f248712ee06ca1d778a357102471186a7e1
abhraimrb <30985012+abhraimrb@users.noreply.github.com>
2023-02-14 18:22:11 +0530

    Update score.py

--- deployment/score.py
+++ deployment/score.py
@@ -1,6 +1,7 @@
 import joblib
 import json
 import numpy as np
+import pandas as pd
 import os
 
 from inference_schema.schema_decorators import input_schema, output_schema
@@ -11,7 +12,7 @@ def init():
     # AZUREML_MODEL_DIR is an environment variable created during deployment.
     # It is the path to the model folder (./azureml-models/$MODEL_NAME/$VERSION)
     # For multiple models, it points to the folder containing all deployed models (./azureml-models)
-    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'sklearn_insurance_claim_prediction_model.pk')
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'insurance_claim_prediction_model.pkl')
     # Deserialize the model file back into a sklearn model.
     model = joblib.load(model_path)
 
@@ -22,10 +23,13 @@ output_sample = np.array([1])
 @output_schema(NumpyParameterType(output_sample))
 def run(data):
     try:
-        result = model.predict(data)
+        data_df = pd.DataFrame(data, columns = ['age', 'sex', 'bmi', 'children', 'smoker', 'region', 'charges'])
+        result = model.predict(data_df)
         # You can return any JSON-serializable object.
         return result.tolist()
     except Exception as e:
         error = str(e)
         return error
 
+
+   
